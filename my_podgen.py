c4a614201b6a4010cd794205d78c03cfef35bef1
ssk8 <ssk8@users.noreply.github.com>
2020-11-12 22:28:27 -0600

    Update my_podgen.py

--- my_podgen.py
+++ my_podgen.py
@@ -1,35 +1,40 @@
 from podgen import Podcast, Episode, Media
+from datetime import timedelta
 # Create the Podcast
 p = Podcast(
-   name="365 days of plops",
-   description="Every shit I take "
-               "you get to hear it",
-   website="http://example.org/animals-alphabetically",
+   name="Chapo Cheaters Club",
+   description="Just taking"
+               "what isn't mine",
+   website="http://www.not-even-real.com",
    explicit=False,
 )
 
-p.image = "https://github.com/ssk8/365days_of_plops/raw/main/pooping.jpg"
+p.image = "http://192.168.1.115:8000/logo.jpg"
 
 p.episodes += [
    Episode(
-     title="Title",
-     media=Media("https://wwww.com", 11932295),
+     title="clover wish",
+     media=Media("http://192.168.1.115:8000/fake_podcast.mp3",size=11680472,
+                       duration=timedelta(minutes=4, seconds=36)), 
      summary="Text",
    ),
 
    Episode(
-     title="Title",
-     media=Media("https://wwww.com", 11932295),
-     summary="Text",
+     title="Chapo Traphouse 471",
+     media=Media("http://192.168.1.115:8000/CTH471_edit_mixdown.output.mp3", size=70623031,
+                       duration=timedelta(hours=1, minutes=38, seconds=4)),
+     summary="Thanks Lars",
    ),
 
       Episode(
-     title="Title",
-     media=Media("https://wwww.com", 11932295),
-     summary="Text",
+     title="Heavenly Caffeteria",
+     
+     media=Media("http://192.168.1.115:8000/caffeteria.mp3", size=2979885,
+                       duration=timedelta(minutes=1, seconds=29)),
+     summary="bunny go fuwafuwa",
    ),
 ]
 
 
 rss = p.rss_str()
-p.rss_file('rss.xml') #, minimize=True)
+p.rss_file('assets/rss.xml') #, minimize=True)
